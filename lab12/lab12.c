8e6a24f3962d51bbbfec22936e0ebf4546355d8d
kumaroid <113765449+kumaroid@users.noreply.github.com>
2022-12-23 19:16:04 +0300

    Update lab12.c

--- lab12/lab12.c
+++ lab12/lab12.c
@@ -3,20 +3,20 @@
 #include <ctype.h>
 #include <string.h>
 
-void read_array(int n, int** values) {
-    for (int i = 0; i < n; ++i) {
+void read_array(int n, int** values){
+    for (int i = 0; i < n; ++i){
         printf("values[%d] = ", i);
         scanf("%d", &((*values)[i]));
     }
 }
 
-void print_array(int n, int* values) {
-    for (int i = 0; i < n; ++i) {
+void print_array(int n, int* values){
+    for (int i = 0; i < n; ++i){
         printf("values[%d] = %d\n", i, values[i]);
     }
 }
 
-void shift_left(int *a, int n) {
+void shift_left(int *a, int n){
     if (0 == n)
         return;
     int first = a[0];
@@ -27,12 +27,12 @@ void shift_left(int *a, int n) {
     a[n-1] = first;
 }
 
-void shift_left_on(int *a, int n, int shift_size) {
-    for (int i = 0; i < shift_size; ++i) {
+void shift_left_on(int *a, int n, int shift_size){
+    for (int i = 0; i < shift_size; ++i){
         shift_left(a, n);
     }
 }
-int main() {
+int main(){
     int n, m;
     int *values;
     
