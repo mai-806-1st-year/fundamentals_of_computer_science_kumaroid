51388c1363c4388b9f734b2891dffecdbd412812
kumaroid <113765449+kumaroid@users.noreply.github.com>
2022-12-23 18:19:41 +0300

    Update laab13.c

--- lab13/laab13.c
+++ lab13/laab13.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <limits.h>
+#define UINT_WIDTH (32)
+#define MAX_ALPHABET ((UINT_MAX >> (UINT_WIDTH - (26U))) + 2)
 
 typedef enum
 {
@@ -16,70 +19,61 @@ uint char_to_set(char c)
     if(c < 'a' || c > 'z')
     {
         return 0;
-    }
-    else
-    {
+    }else{
         return 1u<<(c-'a');
     }
 }
 
-void check_neighboured()
-{
+void check_neighboured(){
     bool flag = false;
     uint set_2;
-    uint set_1 = 67108864;
+    uint set_1 = MAX_ALPHABET;
     Conditions condition = OUTSIDE;
     for (char ch = getchar(); ch != EOF; ch = getchar())
     {
         switch(condition)
         {
             case OUTSIDE:
-                if (isalpha(ch))
+                if (isalpha(ch) || ch == ',')
                 {
                     putchar(ch);
                     set_2 = char_to_set(ch);
                     condition = INSIDE;
                     continue;
-                }
-                else
+                } else
                 {
                     putchar(ch);
                     continue;
                 }
             case INSIDE:
-                if (isalpha(ch))
+                if (isalpha(ch) || ch == ',')
                 {
                     putchar(ch);
                     set_2 = set_2 | char_to_set(ch);
                     continue;
-                }
-                else if( ch == ' ' || ch == '\n')
+                } else if(isspace(ch))
                 {
                     putchar(ch);
-                    if ((set_1 == set_2)&&(set_1 != 67108864)){
+                    if ((set_1 == set_2)&&(set_1 != MAX_ALPHABET)){
                         flag = true;
                     }
                     set_1 = set_2;
                     condition = OUTSIDE;
                     continue;
-                }
-                else
+                } else
                 {
                     putchar(ch);
                     continue;
                 }    
         }
-        if ((set_1 == set_2)&&(set_1 != 67108864))
-        {
+        if ((set_1 == set_2)&&(set_1 != 67108864)){
             flag = true;
         }
     }
     if (!flag)
     {
-        printf("\n\n\nYes!");
-    }
-    else
-    {
+        printf("\n\n\nYES!");
+    } else{
         printf("\n\n\nNO");
     }
 }
@@ -88,5 +82,5 @@ void check_neighboured()
 int main()
 {
     check_neighboured();
-
+    return 0;
 }
